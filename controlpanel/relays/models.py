d32f3bbf2c276d3398c33542f954195a5d92084f
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-08-14 22:57:54 +0300

    add basic relay model

--- controlpanel/relays/models.py
+++ controlpanel/relays/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Relay(models.Model):
+    relay_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.relay_name
