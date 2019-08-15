d04a0fc4ad07350952f78aef163f9375190b7ec2
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-08-15 13:57:01 +0300

    add unique constraint

--- controlpanel/relays/models.py
+++ controlpanel/relays/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Relay(models.Model):
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return self.name
