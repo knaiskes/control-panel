4bd257c1dae1a70a86c06a5e4c90a8b72fda8bd1
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-08-15 13:14:10 +0300

    rename relay_name to name

--- controlpanel/relays/models.py
+++ controlpanel/relays/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Relay(models.Model):
-    relay_name = models.CharField(max_length=15)
+    name = models.CharField(max_length=15)
 
     def __str__(self):
-        return self.relay_name
+        return self.name
