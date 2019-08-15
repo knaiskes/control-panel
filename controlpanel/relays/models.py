d1e11b1d0fc9730d5a6083e1e8a3d4dec7851ff6
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-08-15 16:43:11 +0300

    add state in relays

--- controlpanel/relays/models.py
+++ controlpanel/relays/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Relay(models.Model):
     name = models.CharField(max_length=15, unique=True)
+    state = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
