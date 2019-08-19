68e5a6566c898c2bcbb2ee85e6aaf38b39e7201e
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-08-19 15:21:06 +0300

    Add url and form to update state

--- controlpanel/relays/views.py
+++ controlpanel/relays/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Relay
+from .forms import UpdateStateForm
+from django import forms
 
 def index(request):
     context = {
@@ -7,3 +9,13 @@ def index(request):
         'title': 'Available Relays'
     }
     return render(request, 'relays/index.html', context)
+
+def updateState(request):
+    form = UpdateStateForm()
+
+    if request.method == 'POST':
+        form = UpdateStateForm(request.POST or None)
+        if form.is_valid():
+            state = request.POST.get('state', '')
+            print(state)
+    return render(request, 'relays/updateState.html', {'form': form})
