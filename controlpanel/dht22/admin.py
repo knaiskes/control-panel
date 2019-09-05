20ee90a7b82450e78b7f2c3cc777d989b09562b7
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-09-05 21:41:56 +0300

    Create a foreign model 'data' for dht22 records

--- controlpanel/dht22/admin.py
+++ controlpanel/dht22/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Dht22
+from .models import Dht22, Data
 
 admin.site.register(Dht22)
+admin.site.register(Data)
+
+class DataInline(admin.TabularInline):
+    model = Data
+
+class Dht22Admin(admin.ModelAdmin):
+    inlines = [
+        DataInline,
+    ]
--- controlpanel/dht22/models.py
+++ controlpanel/dht22/models.py
@@ -4,9 +4,16 @@ from django.utils.timezone import now
 class Dht22(models.Model):
     name = models.CharField(max_length=15, unique=True)
     mqtt_topic = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Data(models.Model):
+    dht22_name = models.ForeignKey(Dht22, on_delete=models.CASCADE)
     temperature = models.FloatField(null=True, blank=True, default=None)
     humidity = models.FloatField(null=True, blank=True, default=None)
     date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.dht22_name
+
