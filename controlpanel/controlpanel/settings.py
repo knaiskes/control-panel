632e8e76aaf95a4e104a16e340cacf0e62590401
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-09-21 20:28:16 +0300

    Add settings for templates and static directories in the root of the project

--- controlpanel/controlpanel/settings.py
+++ controlpanel/controlpanel/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'controlpanel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Login urls
 LOGIN_URL = '/accounts/login'
