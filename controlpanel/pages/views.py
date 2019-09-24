9533e96e6bacdcb3b57d933d17b3fc6fbca7c8e3
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-09-24 18:42:35 +0300

    Add basic dashboard page - no contenet yet

--- controlpanel/pages/views.py
+++ controlpanel/pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home_view(request):
     context = { 'title': 'control-panel' }
     return render(request, 'pages/index.html', context)
 
+@login_required
+def dashboard_view(request):
+    context = { 'title': 'dashboard' }
+    return render(request, 'pages/dashboard.html', context)
