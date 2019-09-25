6e2e6756bb7af91a88aabc3bbd569b6669e6d4ad
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-09-26 00:01:37 +0300

    Add username and password in mqtt calls

--- controlpanel/mqtt/models.py
+++ controlpanel/mqtt/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 import paho.mqtt.client as paho
+from json import load
+
+try:
+    with open('../config.json') as json_file:
+        data = load(json_file)
+
+    username = data['mqtt']['username']
+    password = data['mqtt']['password']
+
+except FileNotFoundError:
+    print('Could not open config file')
+
 
 class Mqtt(models.Model):
     broker = "localhost"
@@ -15,6 +27,7 @@ class Mqtt(models.Model):
     def send(self, command):
         try:
             self.client_name = paho.Client()
+            self.client_name.username_pw_set(username=username, password=password)
             self.client_name.on_publish = self.on_publish
             self.client_name.connect(self.broker, self.port)
             ret = self.client_name.publish(self.topic, command)
