a3881b2e9dcb28d1fa798eea02aed95f296ec641
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-10-07 23:03:45 +0300

    Fix test cases

--- controlpanel/dht22/models.py
+++ controlpanel/dht22/models.py
@@ -3,7 +3,7 @@ from django.utils.timezone import now
 
 class Dht22(models.Model):
     name = models.CharField(max_length=15, unique=True, primary_key=True)
-    identifier = models.CharField(max_length=15, unique=True)
+    identifier = models.CharField(max_length=15)
     mqtt_topic = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
